952f513010c6ec9480850c95b683b52674eaddd0
Ashutosh K Sharma <ashutosh2801@gmail.com>
2025-09-02 19:19:23 +0530

    wishlist changes merge when staging merge first time

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\AizUploadController;
 use App\Http\Controllers\TourController;
 use App\Http\Controllers\CityController;
 use App\Http\Controllers\LoginWithOTPController;
+use App\Http\Controllers\SitemapController;
 use App\Http\Controllers\SocialiteController;
 use App\Http\Controllers\StateController;
 use Illuminate\Support\Facades\Route;
@@ -26,6 +27,11 @@ Route::get('/', function () {
         'readmeContent' => Str::markdown(file_get_contents($readmePath)),
     ]);
 });
+
+Route::get('/sitemap.xml', [SitemapController::class, 'index']);
+Route::get('/sitemaps/categories.xml', [SitemapController::class, 'categories']);
+Route::get('/sitemaps/destinations.xml', [SitemapController::class, 'destinations']);
+Route::get('/sitemaps/tours.xml', [SitemapController::class, 'tours']);
 
 Route::post('/tour/single', [TourController::class,'single'])->name('tour.single');
 
