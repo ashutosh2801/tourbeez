15cade224b1d41ba740348f8c7736591f652cdaf
vikasrinviss <vikasrinvi.ss@smartdatainc.net>
2026-06-24 16:10:22 +0530

    dashboard

--- resources/views/components/dashboard.blade.php
+++ resources/views/components/dashboard.blade.php
@@ -126,11 +126,11 @@
             </a>
         </div>
     </div>
-    <div class="col-md-12">
+    <!-- <div class="col-md-12">
             <div class="bg-white border rounded-lg-custom p-3 mb-3">
                 <div id="mountainChart"></div>
             </div>
-        </div>
+        </div> -->
    
 </div>
 
@@ -208,7 +208,7 @@
 </script>
 
 <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
-<script>
+<!-- <script>
     document.addEventListener("DOMContentLoaded", function () {
         var options = {
             chart: {
@@ -274,4 +274,4 @@
         var chart = new ApexCharts(document.querySelector("#mountainChart"), options);
         chart.render();
     });
-</script>
+</script> -->
